48d3118484573ff3beb3b80279f93fab48a4ce0d
Mouad Rhofir <mouad.rhofir@gmail.com>
2022-02-15 23:02:28 +0100

    added document title

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,9 +4,12 @@
 (function(){
    
     function start(){
+        // local variable : 
+        var title = document.title;
         console.log("app started");
         console.info("----------");
-        console.error("this is an error");   
+        console.error("this is an error");  
+        console.log("title "+title); 
     }
     //start();
     //window when you finish loading call the function start
